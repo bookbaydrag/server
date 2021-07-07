36863c663b58b763b8df00fe1734d8621ea93a9e
seannyphoenix <seannyphoenix@gmail.com>
2021-07-06 18:15:55 -0700

    [event] add more population, status codes

--- src/controllers/event.controller.ts
+++ src/controllers/event.controller.ts
@@ -1,36 +1,41 @@
 import { Request, Response } from 'express';
 import { Event } from '../models/index.js';
 
+const populateRules = [{
+  path: 'performers',
+  select: ['_id', 'dragName'],
+},
+{
+  path: 'host',
+  select: ['_id', 'dragName'],
+}];
+
 const createEvent = async (req: Request, res: Response): Promise<void> => {
   try {
     const newEvent = await Event.create(req.body);
-    res.json(newEvent);
+    await Event.populate(newEvent, populateRules);
+    res.status(201).json(newEvent);
   } catch (error) {
-    res.json(error);
+    res.status(400).json(error);
   }
 };
 
 const getAllEvents = async (req: Request, res: Response): Promise<void> => {
   try {
-    const events = await Event.find({})
-        .populate({
-          path: 'performers',
-          select: ['_id', 'dragName'],
-        } );
-    res.json(events);
+    const events = await Event.find({}).populate(populateRules);
+    res.status(200).json(events);
   } catch (error) {
-    res.json(error);
+    res.status(500).json(error);
   }
 };
 
 const getOneEvent = async (req: Request, res: Response): Promise<void> => {
   try {
-    const event = await Event.findById(req.params.id)
-        .populate({
-          path: 'performers',
-          select: ['_id', 'dragName'],
-        } );
-    res.json(event);
+    const event = await Event.findById(req.params.id).populate(populateRules);
+    if (!event) {
+      res.sendStatus(404);
+    }
+    res.status(200).json(event);
   } catch (error) {
     res.json(error);
   }
@@ -42,19 +47,22 @@ const updateEvent = async (req: Request, res: Response): Promise<void> => {
         req.params.id,
         req.body,
         { new: true },
-    );
-    res.json(updatedEvent);
+    ).populate(populateRules);
+    if (!updateEvent) {
+      res.sendStatus(404);
+    }
+    res.status(200).json(updatedEvent);
   } catch (error) {
-    res.json(error);
+    res.status(500).json(error);
   }
 };
 
 const deleteEvent = async (req: Request, res: Response): Promise<void> => {
   try {
     const deleted = await Event.findByIdAndDelete(req.params.id);
-    res.json(deleted);
+    res.status(200).json(!!deleted);
   } catch (error) {
-    res.json(error);
+    res.status(500).json(error);
   }
 };
 
