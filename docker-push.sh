a1be702822669edb92d558107a4026b4053575a9
seannyphoenix <seannyphoenix@gmail.com>
2021-08-25 19:32:34 -0700

    [main] add ecs update-service

--- docker-push.sh
+++ docker-push.sh
@@ -4,3 +4,4 @@ aws ecr get-login-password --region us-west-2 | docker login --username AWS --pa
 docker build -t bbd-server .
 docker tag bbd-server:latest 879735756347.dkr.ecr.us-west-2.amazonaws.com/bbd-server:latest
 docker push 879735756347.dkr.ecr.us-west-2.amazonaws.com/bbd-server:latest
+aws ecs update-service --cluster bbd-cluster --service bbd-server --force-new-deployment > /dev/null
